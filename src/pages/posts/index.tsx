c4345117798b3dfec055d254f24f8ef0a98831aa
Edvaldo Afonso <eadafonso@gmail.com>
2024-11-09 09:04:10 +0100

    feat: create post page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,25 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { MDXProvider } from "@mdx-js/react";
+
+export default function Posts() {
+  const { slug } = useParams();
+  const [Content, setContent] = useState<React.ComponentType | null>(null);
+
+  useEffect(() => {
+    import(`../../posts/${slug}.mdx`)
+      .then((module) => setContent(() => module.default))
+      .catch(() => setContent(null));
+  }, [slug]);
+
+  return (
+    <main className="max-w-[60ch] mx-auto w-full space-y-6 h-[100vh]">
+      <Header showDescription={false} />
+
+      <MDXProvider>{Content ? <Content /> : <p>A carregar ...</p>}</MDXProvider>
+      <Footer />
+    </main>
+  );
+}
